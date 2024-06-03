570939f9b6a9e7ff85008e8a2abef62343d57ed6
Akshay B <beatle02@gmail.com>
2024-06-02 21:51:25 -0400

    Using mustache to generate all index.html

---
+++ index.js
@@ -0,0 +1,40 @@
+const mustache = require("mustache");
+const fs = require("fs");
+const path = require("path");
+
+const baseTemp = fs.readFileSync(
+  path.resolve(__dirname, "./must/base.mustache"),
+  "utf8"
+);
+
+const json = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "./must/data.json"))
+);
+
+if (Array.isArray(json?.data)) {
+  const arr = json.data;
+
+  json.data.forEach((datum, i) => {
+    const htmlContent =
+      i === 0
+        ? mustache.render(baseTemp, datum)
+        : mustache.render(baseTemp, {
+            ...json.data[0],
+            ...datum,
+            cssFiles: datum?.cssFiles
+              ? [...json.data[0].cssFiles, ...datum?.cssFiles]
+              : json.data[0].cssFiles,
+            jsFiles: datum?.jsFiles
+              ? [...json.data[0].jsFiles, ...datum?.jsFiles]
+              : json.data[0].jsFiles,
+          });
+    // console.log(htmlContent);
+    fs.writeFile(`${datum.file}index.html`, htmlContent, (err) => {
+      if (err) {
+        console.log(`Probile while writing to ${datum.file}`);
+        return;
+      }
+      // console.log(err);
+    });
+  });
+}
