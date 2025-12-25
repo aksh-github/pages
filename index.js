babc4e00838502d8a125cd4699527015b4bb9abf
Akshay B <beatle02@gmail.com>
2025-12-25 20:00:13 +0530

    history updated

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ async function content() {
   const inputDirs = [
     // path.resolve(__dirname, "./data/html"),
     // path.resolve(basePath, "./code"),
-    path.resolve(basePath, "./dharm"),
-    // path.resolve(basePath, "./history"),
+    // path.resolve(basePath, "./dharm"),
+    path.resolve(basePath, "./history"),
     // path.resolve(basePath, "./sanskrit"),
   ];
 
